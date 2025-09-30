8b69839ef2fe4e54257476a363b73395b7f763c3
Manjeet Kumar <manjeet@uptics.io>
2025-09-30 18:11:48 +0530

    mocked function

--- src/inngest/functions.js
+++ src/inngest/functions.js
@@ -4,11 +4,15 @@ export const runIt = inngest.createFunction(
     { id: "user-onboarding-communication" },
     { event: "file-upload-event" },
     async ({ event, step }) => {
+
         await step.run("Send welcome email", async () => {
-        await sendEmail({
-            email: event.data.email,
-            template: "welcome",
-        });
+
+            console.log("Event data", event.data);
+
+            // await sendEmail({
+            //     email: event.data.email,
+            //     template: "welcome",
+            // });
         });
     }
 );
